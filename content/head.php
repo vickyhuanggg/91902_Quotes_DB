87349554f88b4fae2ade4f8bb2bb9a2c831c9996
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-09-30 10:41:44 +1300

    Create head.php

---
+++ content/head.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Site featuring short quotes by both famous and not so famous people">
+    <meta name="keywords" content="quotes, famous, author, writer, sportsman, political activist, sayings">
+    <meta name="author" content="Miss Gottschalk">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    
+    <title>Quick Quotes</title>
+    
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="theme/quick_quotes.css"> 
+    <link rel="stylesheet" href="theme/font-awesome.min.css">
+    <link rel="stylesheet" href="theme/auto_complete.css">
+    
+</head>
