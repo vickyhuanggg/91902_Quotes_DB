f71000a0b21ea9c3319f1af09a3db7d06d0dfbed
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-09-30 22:34:39 +1300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <!DOCTYPE html>
+
+<?php
+session_start();
+include("config.php");
+include("functions.php");
+    
+// Connect to database...
+$dbconnect=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+
+if(mysqli_connect_errno()) {
+	echo "Connection failed:".mysqli_connect_error();
+	exit;
+}
+?>
+
+
 <html lang="en">
 
 <?php include("content/head.php"); ?>
