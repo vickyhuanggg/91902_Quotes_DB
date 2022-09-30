f51b39350c0a6f8d96e57b69591190a70509434e
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-09-30 22:34:34 +1300

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 ini_set('display_errors', 'On');  // change to 'Off' when live
 
-define ("DB_NAME","editme"); 
-define ("DB_USERNAME","editme");
-define ("DB_PASSWORD","editme"); 
+define ("DB_NAME","00_l3_quotes"); 
+define ("DB_USERNAME","root");
+define ("DB_PASSWORD",""); 
 define ("DB_HOST","localhost");
 
 ?>
