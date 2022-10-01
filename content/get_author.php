2c9b90e8fdeaba017fa56862cdc5e5987235af7c
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-10-01 21:38:32 +1300

    Create get_author.php

---
+++ content/get_author.php
@@ -0,0 +1,9 @@
+<?php
+    
+    // author name...
+    $first = $find_rs['First'];
+    $middle = $find_rs['Middle'];
+    $last = $find_rs['Last'];
+    
+    $full_name = $first." ".$middle." ".$last; 
+?>
