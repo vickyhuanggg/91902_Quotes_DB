5beb23a9878b5e3d17cb08e2321d6df8f7c83123
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-10-02 21:46:27 +1300

    Update showall.php

--- content/showall.php
+++ content/showall.php
@@ -13,57 +13,21 @@ do {
     
     $quote = preg_replace('/[^A-Za-z0-9.,\s\'\-]/', ' ', $find_rs['Quote']);
     
-    // author name...
-    $first = $find_rs['First'];
-    $middle = $find_rs['Middle'];
-    $last = $find_rs['Last'];
+    include("get_author.php");
     
-    $full_name = $first." ".$middle." ".$last;  
     ?>
+
     <div class="results">
         <p>
 
             <?php echo $quote; ?><br/>
+            <!-- display author name-->
             <a href="index.php?page=author&authorID=<?php echo $find_rs['Author_ID'];?>">
                 <?php echo $full_name; ?>
             </a>
         </p>
         
-        <!--subject tags go here -->
-        <p>
-            <?php 
-                $sub1_ID = $find_rs['Subject1_ID'];
-                $sub2_ID = $find_rs['Subject2_ID'];
-                $sub3_ID = $find_rs['Subject3_ID'];
-                
-                $all_subjects = array($sub1_ID, $sub2_ID, $sub3_ID);
-    
-                //loop through subject ID's and look up the subject name</p>
-                foreach($all_subjects as $subject){
-                    // Get subject name
-                    $sub_sql = "SELECT * FROM `subject` WHERE `Subject_ID` = $subject";
-                    $sub_query = mysqli_query($dbconnect, $sub_sql);
-                    $sub_rs = mysqli_fetch_assoc($sub_query);
-            
-                    if($subject != 0){
-                    ?>
-                
-                <!-- show subjects -->
-                <span class="tag">
-                    <a href="index.php?page=subject&subjectID=<?php echo $sub_rs['Subject_ID']; ?>">
-                        <?php echo $sub_rs['Subject']; ?>
-                    </a>
-                </span> &nbsp;
-            
-            <?php
-                
-                } //end subject exists 
-                    
-                unset($subject);
-                    
-                } // end subject loop
-            ?>
-        </p>
+        <?php include("show_subjects.php");?>
 
     </div>
 
