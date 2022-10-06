51a67e6abf12a1db7a31038babcd85b74427b543
vickyhuanggg <vickyhua@pnghs.school.nz>
2022-10-06 21:00:11 +1300

    Create quote_success.php

---
+++ content/quote_success.php
@@ -0,0 +1,47 @@
+<h2>Success!</h2>
+
+<p>You have put the following quote into the database...</p>
+
+<?php
+
+$quote_ID=$_SESSION['Quote_Success'];
+
+$find_sql = "SELECT * FROM `Quotes`
+JOIN author ON (`author`.`Author_ID` = `Quotes`.`Author_ID`) WHERE `ID` = $quote_ID";
+$find_query = mysqli_query($dbconnect, $find_sql);
+$find_rs = mysqli_fetch_assoc($find_query);
+
+// Loop through results and dislay them...
+do {
+    
+
+    $quote = preg_replace('/[^A-Za-z0-9.,\s\'\-]/', ' ', $find_rs['Quote']);
+    
+    include("get_author.php");
+    
+    ?>
+
+    <div class="results">
+        <p>
+
+            <?php echo $quote; ?><br/>
+            <!-- display author name-->
+            <a href="index.php?page=author&authorID=<?php echo $find_rs['Author_ID'];?>">
+                <?php echo $full_name; ?>
+            </a>
+        </p>
+        
+        <?php include("show_subjects.php");?>
+        
+    </div>
+
+<br />
+
+<?php
+    
+}   // end of display results 'do'
+
+while($find_rs = mysqli_fetch_assoc($find_query))
+
+?>
+
